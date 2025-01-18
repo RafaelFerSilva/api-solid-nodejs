d4c0fdda6ab4e647c3939aeb01697db69c392bf7
rafaelsilva <todosomtodavoz@gmail.com>
2025-01-18 18:44:15 -0300

    Integração com Prisma ORM - Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+prisma.user.create({
+  data: {
+    name: 'Rafael Silva',
+    email: 'rafae@silva.com',
+  },
+})
