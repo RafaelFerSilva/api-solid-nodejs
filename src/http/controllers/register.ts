bcb452b695aa2746e2057732c7ecb965b6a8aaa4
rafaelsilva <todosomtodavoz@gmail.com>
2025-01-28 08:38:59 -0300

    chore: update camelcase

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -12,11 +12,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
