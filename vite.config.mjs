167903743e1c74fb68be5760be9669cfd521daa2
rafaelsilva <todosomtodavoz@gmail.com>
2025-02-02 14:04:54 -0300

    Design Patterns & Testes - Gerando coverage de testes

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,22 @@
-import { defineConfig } from 'vitest/config'
+import { coverageConfigDefaults, defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      exclude: [
+        '**/build/**',
+        '**/env/**',
+        '**/lib/**',
+        '**/http/**',
+        '**/controllers/**',
+        '**/prisma/**',
+        '**/app.ts',
+        '**/server.ts',
+        '**/users.repository.ts',
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
+  },
 })
