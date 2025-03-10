cde3a8130ba84c413a26670b154678a2998092f2
rafaelsilva <todosomtodavoz@gmail.com>
2025-03-10 08:13:53 -0300

    feat: add findById on prisma-users-repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../users.repository'
 
 export class PrismaUsersRepository implements UsersRepository {
@@ -16,6 +16,16 @@ export class PrismaUsersRepository implements UsersRepository {
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
+    })
+
+    return user
+  }
+
+  findById(id: string): Promise<User | null> {
+    const user = prisma.user.findUnique({
+      where: {
+        id,
+      },
     })
 
     return user
