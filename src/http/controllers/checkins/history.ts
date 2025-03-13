fa0f9f9dae7f5db3fb9ed0bd1484ddd70981e4f1
rafaelsilva <todosomtodavoz@gmail.com>
2025-03-13 08:56:48 -0300

    test: should be able to list the history of check-ins

--- src/http/controllers/checkins/history.ts
+++ src/http/controllers/checkins/history.ts
@@ -1,19 +1,22 @@
-import { makeFetchUserCheckInsHistoryUseCase } from '@/use-cases/factories/make-fetch-user-chek-in-history-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { makeFetchUserCheckInsHistoryUseCase } from '@/use-cases/factories/make-fetch-user-chek-in-history-use-case'
 
 export async function history(request: FastifyRequest, reply: FastifyReply) {
-  const checkInHistoryBodySchema = z.object({
+  const checkInHistoryQuerySchema = z.object({
     page: z.coerce.number().min(1).default(1),
   })
 
-  const { page } = checkInHistoryBodySchema.parse(request.body)
-  const featchCheckins = makeFetchUserCheckInsHistoryUseCase()
+  const { page } = checkInHistoryQuerySchema.parse(request.query)
 
-  const { checkIns } = await featchCheckins.execute({
-    userId: request.user.sub,
+  const fetchUserCheckInsHistoryUseCase = makeFetchUserCheckInsHistoryUseCase()
+
+  const { checkIns } = await fetchUserCheckInsHistoryUseCase.execute({
     page,
+    userId: request.user.sub,
   })
 
-  return reply.status(200).send({ checkIns })
+  return reply.status(200).send({
+    checkIns,
+  })
 }
